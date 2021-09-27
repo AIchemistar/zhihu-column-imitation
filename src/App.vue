023ef3545fef41d8e4ae74a014891941cda3b2e7
AIchemistar <wengxing2015@outlook.com>
2021-09-27 16:10:39 +0800

    router开发准备

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,34 @@
 <template>
   <div class="container">
     <global-header :user= "currentUser"></global-header>
-    <!-- <column-list :list="list"></column-list> -->
-    <validate-from @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validateInput
-          :rules="emailRules"
-          v-model="emailVal"
-          placeholder="请输入邮箱地址"
-          type="text"
-          ref="inputRef"
-        />
-        <!-- 上面这个ref可以获取ValidateInput子组件 -->
-        <!-- {{emailVal}}这一行可以用来验证v-model双向绑定 -->
-      </div>
-
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validateInput
-          :rules="passwordRules"
-          v-model="passwordVal"
-          placeholder="请输入密码"
-          type="text"
-        />
-        <!-- {{emailVal}}这一行可以用来验证v-model双向绑定 -->
-      </div>
-      <template #submit> <!-- 使用具名插槽-->
-        <span class="btn btn-danger">
-          Submit
-        </span>
-      </template>
-    </validate-from>
+    <!-- <home></home> -->
+    <login></login>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateFrom from './components/ValidateForm.vue'
+
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'Adrian'
 }
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar:
-    //   'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧,这是的test3专栏，有一段非常有意思的简介，可以更新一下欧,这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
 export default defineComponent({
   name: 'App',
   components: {
-    // ColumnList,
     GlobalHeader,
-    ValidateInput,
-    ValidateFrom
+    // Home,
+    Login
   },
   setup () {
-    const inputRef = ref<any>('')
-    const emailVal = ref('')
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid message'
-      }
-    }
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    const passwordVal = ref('')
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' },
-      {
-        type: 'range',
-        min: { message: '你的密码至少包括六位，不能含有空格', length: 6 },
-        max: { message: '你的密码不能超过24位', length: 24 }
-      }
-    ]
-    const onFormSubmit = (result: boolean) => { // 接收子组件传来的result
-      console.log('表单输入结果为', inputRef.value.validateInput())
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRef,
-      validateEmail,
-      emailRules,
-      passwordRules,
-      emailVal,
-      onFormSubmit,
-      inputRef,
-      passwordVal
+      currentUser
     }
   }
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <validate-form @form-submit="onFormSubmit">
+    <div class="mb-3">
+      <label class="form-label">邮箱地址</label>
+      <validateInput
+        :rules="emailRules"
+        v-model="emailVal"
+        placeholder="请输入邮箱地址"
+        type="text"
+        ref="inputRef"
+      />
+      <!-- 上面这个ref可以获取ValidateInput子组件 -->
+      <!-- {{emailVal}}这一行可以用来验证v-model双向绑定 -->
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">密码</label>
+      <validateInput
+        :rules="passwordRules"
+        v-model="passwordVal"
+        placeholder="请输入密码"
+        type="text"
+      />
+      <!-- {{emailVal}}这一行可以用来验证v-model双向绑定 -->
+    </div>
+    <template #submit> <!-- 使用具名插槽-->
+      <span class="btn btn-danger">
+        Submit
+      </span>
+    </template>
+  </validate-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
+import ValidateForm from '../components/ValidateForm.vue'
+
+export default defineComponent({
+  name: 'Login',
+  components: {
+    ValidateInput,
+    ValidateForm
+  },
+  setup () {
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' },
+      {
+        type: 'range',
+        min: { message: '你的密码至少包括六位，不能含有空格', length: 6 },
+        max: { message: '你的密码不能超过24位', length: 24 }
+      }
+    ]
+    const onFormSubmit = (result: boolean) => { // 接收子组件传来的result
+      console.log('表单输入结果为', result)
+    }
+    return {
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit
+    }
+  }
+})
+</script>
